ef4e9a17176f83cfd7c24ee2c5543a428ff51366
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-01-08 11:36:18 -0800

    added Kaboom blank screen and text to test out framework

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 import kaboom from "./libs/kaboom.mjs"
 
-kaboom()
+kaboom({
+    width: 1280,
+    height: 720,
+    letterbox: true,
+})
+
+const scenes = {
+    menu: () => {
+        add([text("test"), pos(600,500),color(0,0,0)])
+        
+    },
+    controls: () => {
+
+    },
+    main: ()=> {
+
+    }
+}
+
+for (const key in scenes){
+    scene(key, scenes[key])
+}
+
+go("menu")
