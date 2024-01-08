680e2cacc38484c89e3f1c005680ba96ba76edb1
Ghostbitstudios <126435713+Ghostbitstudios@users.noreply.github.com>
2024-01-08 12:25:21 -0800

    added dino asset, created platform, then added gravity

--- main.js
+++ main.js
@@ -4,23 +4,54 @@ kaboom({
     width: 1280,
     height: 720,
     letterbox: true,
+    // background: [245, 193, 51],
 })
 
-const scenes = {
-    menu: () => {
-        add([text("test"), pos(600,500),color(0,0,0)])
+loadSprite("dino", "./assets/lil_Dino.png")
+
+const FLOOR = 100
+
+const Dino = add([
+    sprite("dino"),
+    pos(20,150),
+    scale(2),
+    area(),
+    body(),
+])
+
+//  .jump() when space key is pressed
+onKeyPress("space", () => {
+    Dino.jump()
+})
+
+// add platform
+
+add([
+    rect(width(), FLOOR),
+    outline(4),
+    area(),
+    pos(0, height() - 60),
+    body({isStatic: true}),
+    color(127,200,255),
+])
+
+setGravity(1600)
+
+// const scenes = {
+//     menu: () => {
+//         // add([text("test"), pos(600,500),color(0,0,0)])
         
-    },
-    controls: () => {
+//     },
+//     controls: () => {
 
-    },
-    main: ()=> {
+//     },
+//     main: ()=> {
 
-    }
-}
+//     }
+// }
 
-for (const key in scenes){
-    scene(key, scenes[key])
-}
+// for (const key in scenes){
+//     scene(key, scenes[key])
+// }
 
-go("menu")
+// go("menu")
